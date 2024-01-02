b10a28e831d6b271de50a5785ba978ddc90bdb0b
Danyl Gryn <gryndanya@gmail.com>
2024-01-02 17:22:29 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import MyHello from './components/MyHello.vue'
+</script>
+
+<template>
+  <h1>Blog asdsda</h1>
+  <MyHello />
+</template>
---
+++ src/components/MyHello.vue
@@ -0,0 +1,12 @@
+<script></script>
+
+<template>
+  <p>MY</p>
+  <button>Open</button>
+</template>
+
+<style scoped>
+body {
+  background: red;
+}
+</style>
