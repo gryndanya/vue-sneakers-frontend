1ad90acab60968afed85df8ffd1bd57b4725d627
Danyl Gryn <gryndanya@gmail.com>
2024-01-04 02:35:10 +0200

    Card/Header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
-import MyHello from './components/MyHello.vue'
+import Header from './components/Header.vue'
+import Card from './components/Card.vue'
 </script>
 
 <template>
-  <div class="mb-40">
-    <MyHello title="Кроссовки Puma" :price="6500" is-added />
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+    <Header />
+
+    <div>
+      <Card />
+    </div>
   </div>
 </template>
---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="relative m-10 border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
+  >
+    <img src="/like-1.svg" alt="Like 1" class="absolute top-8 left-8" />
+    <img src="/sneakers/sneakers-1.jpg" alt="Sneaker" />
+
+    <p class="mt-2">Nike Blazer Mid Suede</p>
+
+    <div class="flex justify-between mt-5">
+      <div class="flex flex-col">
+        <span class="text-slate-400"> Price:</span>
+        <span>149.99 $</span>
+      </div>
+
+      <img src="/plus.svg" alt="Plus" />
+    </div>
+  </div>
+</template>
