991dccf849ba60a715b08860ce8fd179db1eb75d
Danyl Gryn <gryndanya@gmail.com>
2024-01-08 10:56:16 +0200

    08.01.2024

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,19 +1,30 @@
+<script setup>
+defineProps({
+  imageUrl: String,
+  title: String,
+  price: Number,
+  isFavourite: Boolean,
+  isAdded: Boolean
+})
+</script>
+
 <template>
   <div
-    class="relative m-10 border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
+    class="relative bg-white border border-slate-200 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
   >
-    <img src="/like-1.svg" alt="Like 1" class="absolute top-8 left-8" />
-    <img src="/sneakers/sneakers-1.jpg" alt="Sneaker" />
+    <img :src="!isFavourite ? '/like-1.svg' : '/like-2.svg'" alt="Like 1" class="absolute top-8 left-8" />
+    
+    <img :src="imageUrl" alt="Sneaker" />
 
-    <p class="mt-2">Nike Blazer Mid Suede</p>
+    <p class="mt-2">{{ title }}</p>
 
     <div class="flex justify-between mt-5">
       <div class="flex flex-col">
         <span class="text-slate-400"> Price:</span>
-        <span>149.99 $</span>
+        <span>{{ price }} $</span>
       </div>
 
-      <img src="/plus.svg" alt="Plus" />
+      <img @click="isAdded = !isAdded" :src="!isAdded ? '/plus.svg' : '/checked.svg'" alt="Plus" />
     </div>
   </div>
 </template>
