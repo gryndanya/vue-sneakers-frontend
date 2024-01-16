24a741afd9bbb53362f8fb92e296348ac91c96a2
Danyl Gryn <gryndanya@gmail.com>
2024-01-16 21:33:03 +0200

    favourite

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,95 @@
 <script setup>
+import { onMounted, ref, reactive, watch } from 'vue'
+import axios from 'axios'
 import Header from './components/Header.vue'
 import CardList from './components/CardList.vue'
 import Drawer from './components/Drawer.vue'
+
+const items = ref([])
+const favorites = ref([])
+
+const filters = reactive({
+  sortBy: 'title',
+  searchQuery: ''
+})
+
+const onChangeSelect = (e) => {
+  filters.sortBy = e.target.value
+}
+
+const onChangeSearch = (e) => {
+  filters.searchQuery = e.target.value
+}
+
+const fetchFavorites = async () => {
+  try {
+    const { data: favorites } = await axios.get(`https://3c0d2296a5b88a3d.mokky.dev/favorites`)
+
+    items.value = items.value.map((obj) => {
+      const favorite = favorites.find((favorite) => favorite.parentId === obj.id)
+
+      if (!favorite) {
+        return obj
+      }
+
+      return {
+        ...obj,
+        isFavourite: true,
+        favoriteId: favorite.id
+      }
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const fetchItems = async () => {
+  try {
+    const params = {
+      sortBy: filters.sortBy
+    }
+
+    if (filters.searchQuery) {
+      params.title = `*${filters.searchQuery}*`
+    }
+    const { data } = await axios.get(`https://3c0d2296a5b88a3d.mokky.dev/items`, { params })
+
+    items.value = data.map((obj) => ({
+      ...obj,
+      isFavourite: false,
+      isAdded: false
+    }))
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+onMounted(async () => {
+  await fetchItems()
+  await fetchFavorites()
+})
+watch(filters, fetchItems)
 </script>
 
 <template>
   <!-- <Drawer /> -->
-  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14 mb-14">
     <Header />
 
     <div class="p-10">
       <div class="flex justify-between items-center">
         <h2 class="text-3xl font-bold mb-8">All Sneakers</h2>
         <div class="flex gap-4">
-          <select class="py-2 px-3 border rounded-md outline-none">
-            <option value="">By name</option>
-            <option value="">By price (less expensive)</option>
-            <option value="">By price (more expensive)</option>
+          <select @change="onChangeSelect" class="py-2 px-3 border rounded-md outline-none">
+            <option value="name">By name</option>
+            <option value="price">By price (less expensive)</option>
+            <option value="-price">By price (more expensive)</option>
           </select>
 
           <div class="relative">
             <img class="absolute left-4 top-3" src="/search.svg" alt="" />
             <input
+              @input="onChangeSearch"
               class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
               type="text"
               placeholder="Search..."
@@ -30,7 +98,9 @@ import Drawer from './components/Drawer.vue'
         </div>
       </div>
 
-      <CardList />
+      <div class="mt-10">
+        <CardList :items="items" />
+      </div>
     </div>
   </div>
 </template>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,6 +1,9 @@
 <script setup>
 import Card from './Card.vue'
 
+defineProps({
+  items: Array
+})
 const onClickAdd = () => {}
 
 const onClickFavourite = () => {}
@@ -9,11 +12,14 @@ const onClickFavourite = () => {}
 <template>
   <div class="grid grid-cols-4 gap-5">
     <Card
-      image-url="/sneakers/sneakers-1.jpg"
-      title="Nike Blazer Mid Suede"
-      :price="129.99"
+      v-for="item in items"
+      :key="item.id"
+      :image-url="item.imageUrl"
+      :title="item.title"
+      :price="item.price"
       :is-added="false"
       :on-click-add="onClickAdd"
+      :is-favourite="false"
       :on-click-favourite="onClickFavourite"
     />
   </div>
